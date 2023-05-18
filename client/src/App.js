f489f80fa8d3c0bdfa7cc463046024847cf6059d
rajabhinav1331 <rajabhinav1331@gmail.com>
2023-05-18 11:40:42 +0530

    Added route path to components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 import { BrowserRouter,Routes,Route} from 'react-router-dom'
 import { Link } from 'react-router-dom';
+import VendorSignup from './components/VendorSignup';
+import VendorLogIn from './components/VendorLogIn';
+import UserSignup from './components/UserSignup';
+import UserLogIn from './components/UserLogIn';
+import LogOut from './components/LogOut';
+import Hero from './components/Hero';
  
 function App() {
   return (
     <div className="App">
     <BrowserRouter>
       <Routes>
-        <Route />
+        <Route path='/vendorSignup' element={<VendorSignup />}/>
+        <Route path='/vendorlogin' element={<VendorLogIn/>}/>
+        <Route path='/usersignup' element={<UserSignup />}/>
+        <Route path='/userlogin' element={<UserLogIn />}/>
+        <Route path='/logout' element={<LogOut />}/>
+        <Route path='/hero' element={<Hero />}/>
       </Routes>
     </BrowserRouter>
     </div>
