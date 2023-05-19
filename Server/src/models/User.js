f3a9446905f9c347e7e30a798fea22c1f25bfb94
rajabhinav1331 <rajabhinav1331@gmail.com>
2023-05-19 14:03:56 +0530

    Created user model

---
+++ Server/src/models/User.js
@@ -0,0 +1,45 @@
+const UserSchema = mongoose.Schema(
+    {
+        Name:{
+            type:String,
+            required:true
+        },
+        Email:{
+            type:String,
+            required:true
+        },
+        Contact:{
+            type:Number,
+            required:true
+        },
+        Password:{
+            type:String,
+            required:true
+        },
+        Cpassword:{
+            type:String,
+            required:true
+        }
+       
+    }
+)
+
+
+
+
+
+const usermodel = mongoose.model("usermodel", UserSchema);
+
+
+ (async function createuser(){
+    let user = {
+        Name:"Abhinav",
+        Email:"rajabhinav321@gmail.com",
+        Contact:"9876543210",
+        Password:123456,
+        Cpassword:123456,
+    }
+
+    let data =  await usermodel.create(user)
+    console.log(data);
+})();
